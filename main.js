ab00b8212f64502bef4e6e15dcfa28196959858c
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2023-05-15 20:05:41 -0300

    Desafio completo

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const cookie = document.querySelector(".cookie");
 const openCookieBtn = document.querySelector("#open__cookie");
+
 const phrases = [
   "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
   "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+  "O aprendizado é como o horizonte: não há limites.",
+  "Não há que ser forte, há que ser flexível.",
+  "Limitações são fronteiras criadas apenas pela nossa mente.",
+  "O cão não ladra por valentia e sim por medo.",
+  "A palavra é prata, o silêncio é ouro.",
+  "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+  "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+  "O homem só envelhece quando os lamentos substituem seus sonhos.",
+  "A persistência realiza o impossível.",
 ];
-let randomPhrase = Math.round(Math.random() * 1);
+
+let randomPhrase = Math.round(Math.random() * phrases.length - 1);
 
 const toggle = () => {
   screen1.classList.toggle("hide");
@@ -20,8 +31,17 @@ const handleClickCookie = () => {
 
 const handleClickOpenCookie = () => {
   toggle();
-  randomPhrase = Math.round(Math.random() * 1);
+  randomPhrase = Math.round(Math.random() * phrases.length - 1);
 };
+
+const handleEnterDocument = (e) => {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleClickOpenCookie();
+  } else if (e.key = "Enter" && screen2.classList.contains("hide")) {
+    handleClickCookie();
+  }
+}
 
 cookie.addEventListener("click", handleClickCookie);
 openCookieBtn.addEventListener("click", handleClickOpenCookie);
+document.addEventListener("keydown", handleEnterDocument);
